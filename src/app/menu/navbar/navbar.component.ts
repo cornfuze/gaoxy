884291e2bc29d48360a19a2bc4bc15925ac08ac2
orlandocalvin <orlandocalvin46@gmail.com>
2024-09-27 10:23:32 +0700

    build android & navbar

--- src/app/menu/navbar/navbar.component.ts
+++ src/app/menu/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EnvironmentInjector, inject } from '@angular/core'
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone'
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonList, IonContent } from '@ionic/angular/standalone'
 import { addIcons } from 'ionicons'
 import { personOutline, homeOutline, desktopOutline, settingsOutline } from 'ionicons/icons'
 
@@ -8,7 +8,7 @@ import { personOutline, homeOutline, desktopOutline, settingsOutline } from 'ion
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel]
+  imports: [IonContent, IonList, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel]
 })
 export class NavbarComponent implements OnInit {
   public environmentInjector = inject(EnvironmentInjector)
@@ -18,4 +18,10 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() { }
+
+  setActive(event: Event): void {
+    const list = document.querySelectorAll('.list');
+    list.forEach((item) => item.classList.remove('active'));
+    (event.currentTarget as HTMLElement).classList.add('active');
+  }
 }
