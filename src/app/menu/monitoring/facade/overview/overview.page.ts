a44abf6be3f1e68d9a9d8f50ba2c7371e88cad65
cornfuze <fr13nds557@gmail.com>
2024-10-04 13:58:58 +0700

    tambah halaman statistic pake chartjs

--- src/app/menu/monitoring/facade/overview/overview.page.ts
+++ src/app/menu/monitoring/facade/overview/overview.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonIcon, IonBadge } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.page.html',
   styleUrls: ['./overview.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBadge, IonIcon, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class OverviewPage implements OnInit {
 
--- src/app/menu/monitoring/facade/overview/statistic/statistic.page.ts
+++ src/app/menu/monitoring/facade/overview/statistic/statistic.page.ts
@@ -1,20 +1,108 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonList,
+  IonItem,
+  IonSelectOption,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,
+} from '@ionic/angular/standalone';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.page.html',
   styleUrls: ['./statistic.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCardTitle,
+    IonCardContent,
+    IonCardHeader,
+    IonCard,
+    IonItem,
+    IonList,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonSelectOption,
+  ],
 })
 export class StatisticPage implements OnInit {
+  selectedLevel: string;
+  selectedGaoxy: string;
+  public buffer = 0.06;
+  public progress = 0;
 
-  constructor() { }
+  constructor() {
+    this.selectedLevel = '';
+    this.selectedGaoxy = '';
 
-  ngOnInit() {
+    setInterval(() => {
+      this.buffer = 0.06;
+      this.progress = 0.63;
+
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.buffer = 0.06;
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 1000);
   }
 
+  ngOnInit() {
+    this.loadChart();
+  }
+
+  loadChart() {
+    const ctx = document.getElementById('trendChart') as HTMLCanvasElement;
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [
+          {
+            label: 'Percentage %',
+            data: [60, 40, 65, 45, 70, 50, 80],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 2,
+            fill: true,
+            tension: 0.4,
+            pointBackgroundColor: 'green',
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: 100,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+  }
 }
